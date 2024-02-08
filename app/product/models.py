8c4831a8999388b4265f983b140fa2dda91c4958
ilgarhasanzada <ilqarhsnzad@gmail.com>
2024-02-08 19:14:23 +0400

    update responsivity

--- app/product/models.py
+++ app/product/models.py
@@ -60,6 +60,7 @@ class IndexSlider(models.Model):
     title = models.CharField(max_length = 500)
     description = models.CharField(max_length = 500)
     image = models.ImageField(upload_to = 'index_slider')
+    mobile_image = models.ImageField(upload_to = 'index_slider_mobile',verbose_name = 'Mobile photo(366x350px)', null = True, blank = True)
 
     def __str__(self):
         return self.title

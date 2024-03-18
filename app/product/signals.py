44d63af5043a176259cc2b94760719b0d3c8a690
ilgarhasanzada <ilqarhsnzad@gmail.com>
2024-03-18 15:46:19 +0400

    update

--- app/product/signals.py
+++ app/product/signals.py
@@ -16,20 +16,20 @@ def update_product_discount(sender, instance, **kwargs):
         instance.product.discount = instance.discount
         instance.product.save()
 
-@receiver(post_save, sender=Order)
-def send_order_email(sender, instance, created, **kwargs):
-    if created and instance.is_wolt:  # Yeni bir sipariş oluşturulduğunda işlevi çalıştırın
-        data = {
-            'order': instance,
-        }
+# @receiver(post_save, sender=Order)
+# def send_order_email(sender, instance, created, **kwargs):
+#     if created and instance.is_wolt:  # Yeni bir sipariş oluşturulduğunda işlevi çalıştırın
+#         data = {
+#             'order': instance,
+#         }
        
-        message = render_to_string('mail.html', data)
+#         message = render_to_string('mail.html', data)
 
-        send_mail(
-            "ecoproduct.az saytından Wolt kuriyer ilə sifariş var. Zəhmət olmasa 30 dəqiqə ərzində sifarişləri hazırlayın",
-            message,
-            settings.EMAIL_HOST_USER,
-            [settings.DEFAULT_FROM_EMAIL],
-            fail_silently=False, html_message=message
-        )   
+#         send_mail(
+#             "ecoproduct.az saytından Wolt kuriyer ilə sifariş var. Zəhmət olmasa 30 dəqiqə ərzində sifarişləri hazırlayın",
+#             message,
+#             settings.EMAIL_HOST_USER,
+#             [settings.DEFAULT_FROM_EMAIL],
+#             fail_silently=False, html_message=message
+#         )   
        

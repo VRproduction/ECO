93ebafa436543276c82caf31361f05316dfdda89
ilgarhasanzada <ilqarhsnzad@gmail.com>
2024-07-10 16:27:05 +0400

    update

--- app/staticfiles/assets/js/notification.js
+++
@@ -1,8 +0,0 @@
-function showNotification(message) {
-    var notification = document.getElementById('notification-message');
-    notification.innerHTML = message;
-    notification.style.display = 'block';
-    setTimeout(function() {
-        notification.style.display = 'none';
-    }, 3000); 
-}

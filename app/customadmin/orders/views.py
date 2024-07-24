1b9a8bf2ce5da5ef3edc2269e0c568775217149e
shabnamadil <sebnemadil1999@gmail.com>
2024-07-24 16:45:54 +0400

    orderadmin updated

--- app/customadmin/orders/views.py
+++ app/customadmin/orders/views.py
@@ -15,7 +15,7 @@ class OrderAdminListView(SuperuserRequiredMixin, ListView):
     template_name = 'orders/order-list.html'
     queryset = Order.objects.all()
 
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+    def get_context_data(self, **kwargs):
         order_types = [
             'Yeni',
             'Paketlənən',
